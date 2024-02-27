1158c687e4c6e0b2b91651a894c462950c55ec9e
KevinAraujo12 <kevinnunes20011210@gmail.com>
2024-02-27 18:22:54 -0300

    ✅:Estudos da faculdade

---
+++ Exercicios4/Att.js
@@ -0,0 +1,18 @@
+function fecharModal() {
+    document.querySelector(".modal").style.display = "none";
+}
+
+function VoltarFormulario() {
+    document.getElementById("fecharBody").style.display
+}
+
+function fecharFormulario() {
+    document.getElementById("fecharBody").innerHTML = "Formulario fechado";
+
+}
+
+function modalMostrar() {
+    document.querySelector(".modal").style.display = "block";
+}
+
+    
